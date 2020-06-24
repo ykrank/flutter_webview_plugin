55bd05250b2951b2a1a2e14699128776c1c81b20
ykrank <ykrank@gmail.com>
2020-06-24 14:30:37 +0800

    Use web view cache lib

--- android/src/main/java/com/flutter_webview_plugin/ObservableWebView.java
+++ android/src/main/java/com/flutter_webview_plugin/ObservableWebView.java
@@ -2,48 +2,50 @@ package com.flutter_webview_plugin;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.webkit.WebView;
 
-public class ObservableWebView extends WebView {
+import com.ryan.github.view.FastWebView;
+import com.ryan.github.view.config.FastCacheMode;
+
+public class ObservableWebView extends FastWebView {
     private OnScrollChangedCallback mOnScrollChangedCallback;
 
-    public ObservableWebView(final Context context)
-    {
+    public ObservableWebView(final Context context) {
         super(context);
+        initFastCache(context);
     }
 
-    public ObservableWebView(final Context context, final AttributeSet attrs)
-    {
+    public ObservableWebView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
+        initFastCache(context);
     }
 
-    public ObservableWebView(final Context context, final AttributeSet attrs, final int defStyle)
-    {
+    public ObservableWebView(final Context context, final AttributeSet attrs, final int defStyle) {
         super(context, attrs, defStyle);
+        initFastCache(context);
+    }
+
+    private void initFastCache(Context context) {
+        setCacheMode(FastCacheMode.FORCE);
     }
 
     @Override
-    protected void onScrollChanged(final int l, final int t, final int oldl, final int oldt)
-    {
+    protected void onScrollChanged(final int l, final int t, final int oldl, final int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
-        if(mOnScrollChangedCallback != null) mOnScrollChangedCallback.onScroll(l, t, oldl, oldt);
+        if (mOnScrollChangedCallback != null) mOnScrollChangedCallback.onScroll(l, t, oldl, oldt);
     }
 
-    public OnScrollChangedCallback getOnScrollChangedCallback()
-    {
+    public OnScrollChangedCallback getOnScrollChangedCallback() {
         return mOnScrollChangedCallback;
     }
 
-    public void setOnScrollChangedCallback(final OnScrollChangedCallback onScrollChangedCallback)
-    {
+    public void setOnScrollChangedCallback(final OnScrollChangedCallback onScrollChangedCallback) {
         mOnScrollChangedCallback = onScrollChangedCallback;
     }
 
     /**
      * Impliment in the activity/fragment/view that you want to listen to the webview
      */
-    public static interface OnScrollChangedCallback
-    {
+    public static interface OnScrollChangedCallback {
         public void onScroll(int l, int t, int oldl, int oldt);
     }
 }
